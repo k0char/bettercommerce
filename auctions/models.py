82505c32fa8b32431c962c5f6fd46e6bafa280f1
K0char <michalkucharewicz0@gmail.com>
2022-10-14 17:13:32 +0200

    init

---
+++ auctions/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
